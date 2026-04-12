9bb032770d570d8e00b8f1003931fc1f85c7089c
yussak <yusuke.sakuraba0213@gmail.com>
2026-04-12 15:50:53 +0900

    テスト時にクリーンアップするよう修正

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -45,6 +45,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.before(:suite) do
+    ActiveRecord::Tasks::DatabaseTasks.truncate_all
+  end
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
